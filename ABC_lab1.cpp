486aaf2f1a59077bd10d9d58ba7bbf6d7b11d92f
Doktorov07 <mdoktorov07@mail.ru>
2026-02-15 18:11:00 +0300

    Add files via upload

---
+++ ABC_lab1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+string tobin(int n) {
+    if (n == 0) return "0";
+
+    string binary = "";
+    while (n > 0) {
+        binary = char('0' + (n % 2)) + binary;
+        n /= 2;
+    }
+    return binary;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int ten, tenn;
+
+    cout << "Число в десятичной: ";
+    cin >> ten;
+    tenn = ten << 1;
+
+    cout << "В двоичной: " << tobin(ten) << endl;
+    cout << "После сдвига = " << tobin(tenn);
+
+    return 0;
+}
